77e43a71065b8d7a23d08b4b1cf1550f0f31e834
Joseph Greve <limejoe517@gmail.com>
2019-03-07 21:26:20 -0500

    It actually works now

---
+++ admin.js
@@ -0,0 +1,5 @@
+// JS that runs on the Better Header Code admin page
+
+jQuery(document).ready(function($) {
+  wp.codeEditor.initialize($('#sl_bhc_option_headercode'), (cm_settings));
+})
--- better_header_code.php
+++ better_header_code.php
@@ -9,9 +9,10 @@
  * Author URI: http://safetyllama.com
  */
 
+// Don't want anyone getting in here that shouldn't be.
+if ( ! defined( 'ABSPATH' ) ) { return; }
 
 // Register Settings
-
 function sl_bhc_register_settings() {
    add_option( 'sl_bhc_option_headercode', '');
    register_setting( 'sl_bhc_options_group', 'sl_bhc_option_headercode', 'sl_bhc_callback' );
@@ -35,9 +36,32 @@ function sl_bhc_plugin_admin_styles() {
     wp_enqueue_style( 'wp_bhc_stylesheet' );
 }
 
+// Hook into our Options page, mostly for adding in CodeMirror support.
+
+add_action( 'current_screen', 'sl_bhc_options_page_hook' );
+function sl_bhc_options_page_hook() {
+    $currentScreen = get_current_screen();
+    if( $currentScreen->id === "settings_page_sl_bhc" ) {
+
+        // Place anything we need to run on our settings page here
+
+        wp_register_script('sl_bhg_admin_script', plugin_dir_url( __FILE__ ) . '/admin.js', array( 'jquery' ), '', true );
+
+        add_action('admin_enqueue_scripts', 'codemirror_enqueue_scripts');
+
+        function codemirror_enqueue_scripts() {
+          $cm_settings['codeEditor'] = wp_enqueue_code_editor(array('type' => 'htmlmixed'));
+          wp_localize_script('sl_bhg_admin_script', 'cm_settings', $cm_settings);
+          wp_enqueue_script('sl_bhg_admin_script');
+        }
+    }
+}
+
 // Options Page
 function sl_bhc_options_page()
 {
+
+  // Options Page Content
   ?>
   <div class="wrap">
   <h1>Better Header Code</h1>
