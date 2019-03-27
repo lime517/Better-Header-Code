ff1de51fe536fc0080942784fb6a7cc474221ee8
Joseph Greve <limejoe517@gmail.com>
2019-03-26 21:48:05 -0400

    RC 2
    Everything works now!

---
+++ README.txt
@@ -0,0 +1,57 @@
+=== Better Header Code ===
+Contributors: safetyllama
+Tags: header code, analytics code, insert header code, google analytics, inject code, inject header code, inject code, better header code
+Requires at least: 4.0
+Tested up to: 5.1
+Stable tag: trunk
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+The easiest way to add code to the header. Great for Google Analytics and more!
+
+== Description ==
+
+The easiest way to add code to the header. Great for Google Analytics and more! Supports scripts, css, or any valid HTML.
+
+**Supported Languages**
+
+* English
+
+== Installation ==
+
+Install Better Header Code the same you would any other plugin!
+
+Dashboard Method:
+
+1. Login to your WordPress admin and go to Plugins -> Add New
+2. Type "Better Search Replace" in the search bar and select this plugin
+3. Click "Install", and then "Activate Plugin"
+
+
+Upload Method:
+
+1. Unzip the plugin and upload the "better-header-code" folder to your 'wp-content/plugins' directory
+2. Activate the plugin through the Plugins menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Using Better Header Code =
+
+Once activated, Better Header Code will add a page under the "Settings" menu page in your WordPress admin.
+
+= Is my host supported? =
+
+Yes! This plugin should be compatible with any host.
+
+= Can I damage my site with this plugin? =
+
+Not permanently! If you save bad code, you can easily remove it.
+
+== Screenshots ==
+
+1. The Better Header Code plugin menu in the 'Tools' menu.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release!
--- better_header_code.php
+++ better_header_code.php
@@ -10,49 +10,59 @@
  */
 
 // Don't want anyone getting in here that shouldn't be.
-if ( ! defined( 'ABSPATH' ) ) { return; }
+if (! defined('ABSPATH')) {
+    return;
+}
 
 // Register Settings
-function sl_bhc_register_settings() {
-   add_option( 'sl_bhc_option_headercode', '');
-   register_setting( 'sl_bhc_options_group', 'sl_bhc_option_headercode', 'sl_bhc_callback' );
+function sl_bhc_register_settings()
+{
+    add_option('sl_bhc_option_headercode', '');
+    register_setting('sl_bhc_options_group', 'sl_bhc_option_headercode', 'sl_bhc_callback');
+    add_option('sl_bhc_option_header_enable', '');
+    register_setting('sl_bhc_options_group', 'sl_bhc_option_header_enable', 'sl_bhc_callback');
 }
-add_action( 'admin_init', 'sl_bhc_register_settings' );
+add_action('admin_init', 'sl_bhc_register_settings');
 
 // Register Options Page
-function sl_bhc_register_options_page() {
-  $page = add_options_page('Better Header Code', 'Better Header Code', 'manage_options', 'sl_bhc', 'sl_bhc_options_page');
-  add_action( 'admin_print_styles-' . $page, 'sl_bhc_plugin_admin_styles' );
+function sl_bhc_register_options_page()
+{
+    $page = add_options_page('Better Header Code', 'Better Header Code', 'manage_options', 'sl_bhc', 'sl_bhc_options_page');
+    add_action('admin_print_styles-' . $page, 'sl_bhc_plugin_admin_styles');
 }
 add_action('admin_menu', 'sl_bhc_register_options_page');
 
 // Add CSS
-add_action( 'admin_init', 'sl_bhc_css' );
+add_action('admin_init', 'sl_bhc_css');
 
-function sl_bhc_css() {
-    wp_register_style( 'wp_bhc_stylesheet', plugins_url( 'main.css', __FILE__ ) );
+function sl_bhc_css()
+{
+    wp_register_style('wp_bhc_stylesheet', plugins_url('main.css', __FILE__));
 }
-function sl_bhc_plugin_admin_styles() {
-    wp_enqueue_style( 'wp_bhc_stylesheet' );
+function sl_bhc_plugin_admin_styles()
+{
+    wp_enqueue_style('wp_bhc_stylesheet');
 }
 
 // Hook into our Options page, mostly for adding in CodeMirror support.
 
-add_action( 'current_screen', 'sl_bhc_options_page_hook' );
-function sl_bhc_options_page_hook() {
+add_action('current_screen', 'sl_bhc_options_page_hook');
+function sl_bhc_options_page_hook()
+{
     $currentScreen = get_current_screen();
-    if( $currentScreen->id === "settings_page_sl_bhc" ) {
+    if ($currentScreen->id === "settings_page_sl_bhc") {
 
         // Place anything we need to run on our settings page here
 
-        wp_register_script('sl_bhg_admin_script', plugin_dir_url( __FILE__ ) . '/admin.js', array( 'jquery' ), '', true );
+        wp_register_script('sl_bhg_admin_script', plugin_dir_url(__FILE__) . '/admin.js', array( 'jquery' ), '', true);
 
         add_action('admin_enqueue_scripts', 'codemirror_enqueue_scripts');
 
-        function codemirror_enqueue_scripts() {
-          $cm_settings['codeEditor'] = wp_enqueue_code_editor(array('type' => 'htmlmixed'));
-          wp_localize_script('sl_bhg_admin_script', 'cm_settings', $cm_settings);
-          wp_enqueue_script('sl_bhg_admin_script');
+        function codemirror_enqueue_scripts()
+        {
+            $cm_settings['codeEditor'] = wp_enqueue_code_editor(array('type' => 'htmlmixed'));
+            wp_localize_script('sl_bhg_admin_script', 'cm_settings', $cm_settings);
+            wp_enqueue_script('sl_bhg_admin_script');
         }
     }
 }
@@ -61,14 +71,16 @@ function sl_bhc_options_page_hook() {
 function sl_bhc_options_page()
 {
 
-  // Options Page Content
-  ?>
+  // Options Page Content ?>
   <div class="wrap">
   <h1>Better Header Code</h1>
   <form method="post" action="options.php">
-  <?php settings_fields( 'sl_bhc_options_group' ); ?>
+  <?php settings_fields('sl_bhc_options_group'); ?>
   <h3>Header Code</h3>
-  <p>Place any HTML here, including scripts and styles.</p>
+  <p>You can place any HTML here, including scripts and styles.</p>
+  <input type="checkbox" id="sl_bhc_option_header_enable" name="sl_bhc_option_header_enable"  <?php if (get_option('sl_bhc_option_header_enable') == 'on' || get_option('sl_bhc_option_header_enable') == true) {
+      echo 'checked';
+  }; ?>/>Enable Header Code?<br><br>
   <table>
   <tr valign="top">
   <td><textarea cols="80" rows="6" type="text" id="sl_bhc_option_headercode" name="sl_bhc_option_headercode"><?php echo get_option('sl_bhc_option_headercode') ?></textarea></td>
@@ -76,14 +88,21 @@ function sl_bhc_options_page()
   </table>
   <?php  submit_button(); ?>
   </form>
+
+  <div class="credits">
+    <p><i>Thanks for using Better Header Code by <a target="_blank" href="http://safetyllama.com">Safety Llama</a>!</i></p>
+  </div>
   </div>
 <?php
 }
 
 
 // Add Header code to the actual header
-function sl_bhc_inject_code() {
-  echo get_option('sl_bhc_option_headercode');
+function sl_bhc_inject_code()
+{
+    if (get_option('sl_bhc_option_header_enable') == 'on' || get_option('sl_bhc_option_header_enable') == true) {
+        echo get_option('sl_bhc_option_headercode');
+    }
 }
 add_action('wp_head', 'sl_bhc_inject_code');
 
