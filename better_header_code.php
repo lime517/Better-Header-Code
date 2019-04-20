bb73eb0a5096c786493d7c31905dc2ab7089a19e
Joseph Greve <limejoe517@gmail.com>
2019-04-20 14:15:25 -0400

    Sanitization and Validation Updates

--- better_header_code.php
+++ better_header_code.php
@@ -18,11 +18,29 @@ if (! defined('ABSPATH')) {
 function sl_bhc_register_settings()
 {
     add_option('sl_bhc_option_headercode', '');
-    register_setting('sl_bhc_options_group', 'sl_bhc_option_headercode', 'sl_bhc_callback');
+    register_setting('sl_bhc_options_group', 'sl_bhc_option_headercode', 'sl_bhc_headercode_sanitize');
     add_option('sl_bhc_option_header_enable', '');
-    register_setting('sl_bhc_options_group', 'sl_bhc_option_header_enable', 'sl_bhc_callback');
+    register_setting('sl_bhc_options_group', 'sl_bhc_option_header_enable', 'sl_bhc_headercode_enable_sanitize');
 }
 add_action('admin_init', 'sl_bhc_register_settings');
+
+// Sanitize functions, for when stuff gets submitted. on the options page.
+// Code
+function sl_bhc_headercode_sanitize ($content) {
+  // We can't really sanitize this HTML. We INTENTIONALLY want the user to be able to put whatever they want in here.
+  $content;
+}
+
+// On/Off switch
+function sl_bhc_headercode_enable_sanitize ($content) {
+  // This shouldn't be anything other than OFF or ON.
+  if ($content != 'on' || $content != 'off') {
+    $content = 'off';
+  }
+
+  return $content;
+}
+
 
 // Register Options Page
 function sl_bhc_register_options_page()
@@ -83,7 +101,7 @@ function sl_bhc_options_page()
   }; ?>/>Enable Header Code?<br><br>
   <table>
   <tr valign="top">
-  <td><textarea cols="80" rows="6" type="text" id="sl_bhc_option_headercode" name="sl_bhc_option_headercode"><?php echo get_option('sl_bhc_option_headercode') ?></textarea></td>
+  <td><textarea cols="80" rows="6" type="text" id="sl_bhc_option_headercode" name="sl_bhc_option_headercode"><?php echo esc_html(get_option('sl_bhc_option_headercode')) ?></textarea></td>
   </tr>
   </table>
   <?php  submit_button(); ?>
@@ -101,7 +119,7 @@ function sl_bhc_options_page()
 function sl_bhc_inject_code()
 {
     if (get_option('sl_bhc_option_header_enable') == 'on' || get_option('sl_bhc_option_header_enable') == true) {
-        echo get_option('sl_bhc_option_headercode');
+        echo (get_option('sl_bhc_option_headercode'));
     }
 }
 add_action('wp_head', 'sl_bhc_inject_code');
